ce443394a794e475e6377e298f50f26126cde94f
Antonio Andelic <antonio.andelic@microblink.com>
2020-04-07 21:55:54 +0200

    Using memcpy

--- Layers/LeakyReluLayer.cpp
+++ Layers/LeakyReluLayer.cpp
@@ -9,14 +9,14 @@
 
 #include <cassert>
 #include <cstdio>
+#include <cstring>
 
 LeakyReluLayer::LeakyReluLayer(const void* buffer, std::size_t size )
 {
     assert( size == sizeof( float ) + sizeof( std::size_t ) );
-    const float* alpha = reinterpret_cast< const float* >( buffer );
-    mAlpha = *alpha;
-    ++alpha;
-    mInputSize = *reinterpret_cast< const std::size_t* >( alpha );
+    memcpy( &mAlpha, buffer, sizeof( float ) );
+    buffer += sizeof( float );
+    memcpy( &mInputSize, buffer, sizeof( std::size_t ) );
 }
 
 void LeakyReluLayer::configure(const nvinfer1::Dims* inputs, int nbInputs, const nvinfer1::Dims* outputs, int nbOutputs, int maxBatchSize )
@@ -30,11 +30,9 @@ void LeakyReluLayer::configure(const nvinfer1::Dims* inputs, int nbInputs, const
 void LeakyReluLayer::serialize(void* buffer )
 {
     // memcpy
-    float* f = reinterpret_cast< float* >( buffer );
-    *f = mAlpha;
-    ++f;
-    std::size_t* sizePtr = reinterpret_cast< std::size_t* >( f );
-    *sizePtr = mInputSize;
+    memcpy( buffer, &mAlpha, sizeof( float ) );
+    buffer += sizeof( float );
+    memcpy( buffer, &mInputSize, sizeof( std::size_t ) );
 }
 
 int LeakyReluLayer::enqueue(int batchSize, const void* const* inputs, void** outputs, void* workspace, cudaStream_t stream )
